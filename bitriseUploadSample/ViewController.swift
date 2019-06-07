330340c8d269ce93c4d3da4946d7ae6367d5b692
keisuke shingaki <kei4eva4@gmail.com>
2019-06-08 01:31:51 +0900

    Initial Commit

---
+++ bitriseUploadSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bitriseUploadSample
+//
+//  Created by keisuke shingaki on 2019/06/08.
+//  Copyright © 2019 keisuke shingaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
